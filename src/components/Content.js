106c74379d6500d8eaba4e883d10ab5234f1004f
reinaldomoraes <reinaldosmoraes@hotmail.com>
2018-08-19 17:35:12 -0300

    Envio por email ao submeter o formulário

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import axios from 'axios';
+
 import '../App.css';
 
 class Content extends Component {
+
+	constructor() {
+		super()
+
+		this.state = {
+			name: '',
+			email: '',
+			message: ''
+		}
+
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
+	async handleSubmit(e) {
+		e.preventDefault()
+
+		const {name, email, message} = this.state
+
+		const form = await axios.post('/api/form', {
+			name,
+			email,
+			message
+		})
+	}
+
   render() {
     return (
-      <div className="content">
-        <form id='name-form'>
-					<label>
-						Name:
-						<input type="text" name="name" />
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
+			<div className="content">
+				<Form onSubmit={this.handleSubmit}>
+					<FormGroup>
+						<Label for="name">Nome:</Label>
+						<Input 
+							type="text"
+							name="name"
+							onChange={this.handleChange} />
+					</FormGroup>
+					
+					<FormGroup>
+						<Label for="email">Email:</Label>
+						<Input 
+							type="email"
+							name="email"
+							onChange={this.handleChange} />
+					</FormGroup>
 
-      </div>
+					<FormGroup>
+						<Label for="message">Mensagem:</Label>
+						<Input 
+							type="textarea"
+							name="message"
+							onChange={this.handleChange} />
+					</FormGroup>
+
+					<Button>Submit</Button>
+				</Form>
+			</div>
     );
   }
 }
