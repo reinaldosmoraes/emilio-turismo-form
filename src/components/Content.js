42dd59be087256ef4321689fb230edfc93466125
reinaldomoraes <reinaldosmoraes@hotmail.com>
2018-08-20 21:02:57 -0300

    Adição dos campos data de chegada e número de pessoas

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import axios from 'axios';
+import DatePicker from 'react-date-picker';
 
 import '../App.css';
 
@@ -12,12 +13,16 @@ class Content extends Component {
 		this.state = {
 			name: '',
 			email: '',
-			message: ''
+			message: '',
+			departureDate: new Date()
 		}
 
 		this.handleChange = this.handleChange.bind(this)
 		this.handleSubmit = this.handleSubmit.bind(this)
 	}
+
+	//date picker
+	onChange = date => this.setState({ date })
 
 	handleChange = e => {
 		this.setState({ [e.target.name]: e.target.value })
@@ -26,11 +31,12 @@ class Content extends Component {
 	async handleSubmit(e) {
 		e.preventDefault()
 
-		const {name, email, message} = this.state
+		const {name, email, departureDate, message} = this.state
 
 		const form = await axios.post('/api/form', {
 			name,
 			email,
+			departureDate,
 			message
 		})
 	}
@@ -56,6 +62,35 @@ class Content extends Component {
 					</FormGroup>
 
 					<FormGroup>
+						<Label for="departureDate">Data de partida:</Label>
+						<p>
+							<DatePicker
+								onChange={this.onChange}
+								value={this.state.date} />
+						</p>
+					</FormGroup>
+
+					<FormGroup>
+						<Label for="arrivalDate">Data de chegada:</Label>
+						<p>
+							<DatePicker
+								onChange={this.onChange}
+								value={this.state.date} />
+						</p>
+					</FormGroup>
+
+					<FormGroup>
+						<Label for="numberOfPeople">Número de Pessoas</Label>
+						<Input type="select" name="numberOfPeople" id="numberOfPeople">
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</Input>
+						</FormGroup>
+
+					<FormGroup>
 						<Label for="message">Mensagem:</Label>
 						<Input 
 							type="textarea"
@@ -63,8 +98,9 @@ class Content extends Component {
 							onChange={this.handleChange} />
 					</FormGroup>
 
-					<Button>Submit</Button>
+					<Button>Solicitar Orçamento</Button>
 				</Form>
+
 			</div>
     );
   }
