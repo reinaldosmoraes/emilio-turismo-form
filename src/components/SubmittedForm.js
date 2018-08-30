b4aeb04ee1ce5a5d4dfecd88151e3f0696295849
Reinaldo Moraes <reinaldo.moraes@b2wdigital.com>
2018-08-30 16:27:17 -0300

    Criação do componente da tela de Obrigado ao submeter o formulário

---
+++ src/components/SubmittedForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SubmittedForm = props => {
+    return (
+        <div className="content">
+            <h1>Formulário Enviado com sucesso</h1>
+            <h3>O formulário foi enviado e está sendo gerado um orçamento que será enviado por email.</h3>
+            
+        </div>
+    );
+};
+
+export default SubmittedForm;
