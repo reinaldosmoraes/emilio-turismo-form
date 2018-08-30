b09203ffee19b869a58dabc35ddb38302525d518
Reinaldo Moraes <reinaldo.moraes@b2wdigital.com>
2018-08-30 11:40:16 -0300

    Aumento do númeor máximo de pessoas no orçamento personalizado para 10 pessoas

--- src/components/Content.js
+++ src/components/Content.js
@@ -102,6 +102,11 @@ class Content extends Component {
 						<option>3</option>
 						<option>4</option>
 						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
 					</Input>
 				</FormGroup>
 
