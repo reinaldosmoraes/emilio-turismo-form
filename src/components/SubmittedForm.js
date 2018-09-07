759f983ce17a90eacf772a772dec43349746546d
reinaldomoraes <reinaldosmoraes@hotmail.com>
2018-09-07 12:06:51 -0300

    Ajustes na tela de Obrigado

--- src/components/SubmittedForm.js
+++ src/components/SubmittedForm.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const SubmittedForm = props => {
     return (
-        <div className="content">
-            <h1>Formulário Enviado com sucesso</h1>
-            <h3>O formulário foi enviado e está sendo gerado um orçamento que será enviado por email.</h3>
-            
+        <div className="submitted-form">
+            <h3 className="sub-title">Formulário Enviado com sucesso</h3>
+            <p>Será gerado um orçamento e enviado para o email cadastrado em um prazo de 24h.</p>
+            <p>Para dúvidas ou mais informações entre em contato com o email contato@emilioturismo.com.br 
+            ou ligue para (21) 38513146 no horário comercial.</p>
+            <p>Conheça a agência e tenha mais informações no nosso site <a href="https://emilioturismo.com.br/">www.emilioturismo.com.br</a>.</p>
         </div>
     );
 };
