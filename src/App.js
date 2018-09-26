84254de69e1b2bf4d7e89c5e90ecb8587400c15d
reinaldomoraes <reinaldosmoraes@hotmail.com>
2018-09-26 00:26:46 -0300

    Início da migração para tabs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Menu from './components/Menu';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import './App.css';
+import Tabs from './components/Tabs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Menu />
+   
 
-        <Content />
+        <Tabs />
 
         <Footer />
       </div>
